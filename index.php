c016a98ea59a342e5474fc59af0fef0eefd9e280
Amelia Wietting <ask.wietting@gmail.com>
2017-01-21 04:10:45 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+
+<html>
+<head>
+<title>Easy Binary Forms a Responsive Widget Template :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Easy Binary Forms Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href='//fonts.googleapis.com/css?family=Text+Me+One' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+<!-- //web font -->
+</head>
+<body>
+<h1>Time to get to work!!!</h1>
+
+<!-- copyright -->
+	<div class="copyright w3-agile">
+		<p> © 2017 AnyVote . All rights reserved | CSS by <a href="http://w3layouts.com/" target="_blank">W3layouts</a></p>
+	</div>
+	<!-- //copyright --> 
+</body>
+</html>
