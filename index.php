0ef4fa1ef5aa5fdce50fb7a521b8c3bb71127d44
Butterfly-3Kisses <Amelia.Leah.W@Gmail.com>
2017-01-20 23:21:36 -0600

    Formatting for this to be the landing page

--- index.php
+++ index.php
@@ -1,74 +1,57 @@
 <!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+   Author: W3layouts
+   Author URL: http://w3layouts.com
+   License: Creative Commons Attribution 3.0 Unported
+   License URL: http://creativecommons.org/licenses/by/3.0/
+   -->
 <!DOCTYPE html>
 <html>
-<head>
-<title>Test Survey Form</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Lorem Ipsom" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- Custom Theme files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<!-- //Custom Theme files -->
-<!-- web font -->
-<link href='//fonts.googleapis.com/css?family=Text+Me+One' rel='stylesheet' type='text/css'>
-<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-
-<!-- //web font -->
-</head>
-<body>
-<h1>First Design!</h1>
-<div class="main-agileits">
-<!--form-stars-here-->
-		<div class="left-form-w3-agile">
-			<h2>Login</h2>
-								<form action="#" method="post">
-									<p>Text</p>
-									<input type="text" class="text" name="text" placeholder="" required=""/>
-									<p>Other text</p>
-									<input type="text" class="text" name="text2" placeholder="" required=""/>
-									
-											<input type="submit" value="GO"> 
-								</form>
-		</div>
-<!--//form-ends-here-->
-		<div class="right-map-w3-agile">
-		<h2>More stuff!</h2>
-			<form action="#" method="post">
-			<div class="inputs-w3ls">
-				<p>Junk!</p>
-					<input type="text" name="Name" placeholder="" required="">
-			</div>
-			<div class="inputs-w3ls">
-				<p>Other!</p>
-					<input type="email" class="email" name="Email" placeholder="" required=""/>
-			</div>
-			<div class="inputs-w3ls">
-				<p>COOL QUESTION</p>
-					<input type="password" class="password" name="Password" placeholder="" required=""/>
-			</div>
-			<div class="inputs-w3ls">
-				<p>WOW SECRET</p>
-					<input type="password" class="password" name="Confirm Password" placeholder="" required=""/>
-			</div>		
-					<label class="anim">
-					<input type="checkbox" class="checkbox">
-						<span> I accept the terms of use</span> 
-					</label> 
-						<input class="register" type="submit" value="GO">  
-			</form> 
-		</div>
-<div class="clear"></div>
-</div>
-<!-- copyright -->
-	<div class="copyright w3-agile">
-		<p> � 2017 AnyBit . All rights reserved | CSS by <a href="http://w3layouts.com/" target="_blank">W3layouts</a></p>
-	</div>
-	<!-- //copyright --> 
-</body>
+   <head>
+      <title>Test Survey Form</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="keywords" content="Lorem Ipsom" />
+      <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+      <!-- Custom Theme files -->
+      <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+      <!-- //Custom Theme files -->
+      <!-- web font -->
+      <link href='//fonts.googleapis.com/css?family=Text+Me+One' rel='stylesheet' type='text/css'>
+      <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+      <!-- //web font -->
+   </head>
+   <body>
+      <h1>First Design!</h1>
+      <div class="main-agileits">
+         <!--form-stars-here-->
+         <div class="left-form-w3-agile">
+            <h2>Intro</h2>
+            <div class="panel">
+               <div>
+                  <p>What is this all about?</p>
+                  <h3>Lorem Ipsom</h3>
+               </div>
+               <p>Other text</p>
+               <input type="submit" value="Read More"> 
+            </div>
+         </div>
+         <div class="right-map-w3-agile">
+            <h2>Terms and Conditions</h2>
+            <div class="panel">
+               <div>
+                  <p>What is this all about?</p>
+                  <h3>Lorem Ipsom</h3>
+               </div>
+               <p>Other text</p>
+               <input type="submit" value="Read More"> 
+            </div>
+         </div>
+         <div class="clear"></div>
+      </div>
+      <!-- copyright -->
+      <div class="copyright w3-agile">
+         <p> � 2017 AnyBit . All rights reserved | CSS by <a href="http://w3layouts.com/" target="_blank">W3layouts</a></p>
+      </div>
+      <!-- //copyright --> 
+   </body>
 </html>
